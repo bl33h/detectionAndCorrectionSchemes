0c3fa268478952466eef927e073cc95d9bae6b00
bl33h <per21371@uvg.edu.gt>
2024-07-25 00:29:18 -0600

    hamming receiver

---
+++ src/receiver/hamming.py
@@ -0,0 +1,25 @@
+def calculateParity(bits):
+    count = sum(1 for bit in bits if bit == '1')
+    
+    # even parity: if count is odd, parity is 1, otherwise it's 0
+    return '1' if count % 2 != 0 else '0'
+
+def hammingCheck():
+    # request the message from the user
+    receivedMessage = input("-> enter the message (frame + parity bit): ")
+    
+    # extract the frame and the received parity bit
+    data = receivedMessage[:-1]
+    receivedParity = receivedMessage[-1]
+    
+    # calculate the parity bit for the string
+    calculatedParity = calculateParity(data)
+    
+    # check if the parity bits match
+    if receivedParity != calculatedParity:
+        print("!error detected: the message is discarded.")
+    else:
+        # display the message without the parity bit
+        print(f"✓message received correctly: {data}")
+
+hammingCheck()
