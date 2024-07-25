00a33f1cb9aa32df041b84c4b86a9d712ae4422d
Mendezg1 <87867522+Mendezg1@users.noreply.github.com>
2024-07-25 16:56:01 -0600

    sender fixed

--- src/sender/crc32.cpp
+++ src/sender/crc32.cpp
@@ -11,7 +11,7 @@ int mostSignificantBitPosition(const std::string& bitString) {
 }
 
 void XOR(std::string& data, const std::string& polynomial, const int pos){
-    for(int i = 0; i<32; i++){
+    for(int i = 0; i<polynomial.size(); i++){
         if(data[i + pos] == polynomial[i]){
             data[i+pos] = '0';
         }
@@ -24,23 +24,25 @@ void XOR(std::string& data, const std::string& polynomial, const int pos){
 std::string crc32(const std::string& data, const std::string& polynomial, const int data_len) {
     std::string result = data;
     int pos = 0;
-    while((data.size() - pos) >= 32){
+    while((data.size() - pos) >= polynomial.size() - 1){
         XOR(result, polynomial, pos);
         pos = mostSignificantBitPosition(result);
     }
-    return result.substr(data_len, 32);
+    return result.substr(data.size() - (polynomial.size() - 1));
 }
 
 int main() {
-    std::string original_data = "11011"; 
+    std::string original_data = "11010"; 
+    std::string polynomial = "111100000000000000000000000000001"; 
     int initial_pos = original_data.size() - 1;
-    std::string data = original_data.append(32,'0');
-    std::string polynomial = "10000010011000001000111001011000"; 
+    std::string data = original_data;
+    data.append(polynomial.size() - 1, '0');
+    
 
     std::string checksum = crc32(data, polynomial, initial_pos);
     std::string crc = original_data + checksum;
     std::cout << "CRC32: " << crc << std::endl;
 
+    // Result = CRC32: 1101001000000000000000000000000010110
     return 0;
-    
 }
